b630ac1ea4f4cb1563f5ad4c3eee7cf093d1f071
sriram <tsriram333@gmail.com>
2019-12-24 09:09:48 +0530

    first commit

---
+++ meals_app/lib/dummy_Data.dart
@@ -0,0 +1,57 @@
+import 'models/category_Model.dart';
+import 'package:flutter/material.dart';
+
+class CategoryData {
+  List<Category> data = [
+    Category(
+      id: "c1",
+      color: Colors.grey,
+      title: "Indian",
+    ),
+    Category(
+      id: "c2",
+      color: Colors.purple,
+      title: "Burgers",
+    ),
+    Category(
+      id: "c3",
+      color: Colors.yellow,
+      title: "Biryani",
+    ),
+    Category(
+      id: "c4",
+      color: Colors.blue,
+      title: "Pasta",
+    ),
+    Category(
+      id: "c5",
+      color: Colors.grey,
+      title: "noodles",
+    ),
+    Category(
+      id: "c6",
+      color: Colors.purple,
+      title: "fast food",
+    ),
+    Category(
+      id: "c7",
+      color: Colors.yellow,
+      title: "rice",
+    ),
+    Category(
+      id: "c8",
+      color: Colors.blue,
+      title: "roti",
+    ),
+    Category(
+      id: "c9",
+      color: Colors.yellow,
+      title: "mutton",
+    ),
+    Category(
+      id: "c10",
+      color: Colors.blue,
+      title: "chicken",
+    ),
+  ];
+}
