f6d4eaa56e4ace35ef77b293b8b00231ab2c18b4
sriram <tsriram333@gmail.com>
2019-12-25 13:07:39 +0530

    Second commit-3

---
+++ meals_app/lib/screens/single_Food_Item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SingleFoodItem extends StatelessWidget {
+  static final String routeName = "/SingleFoodItem";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
